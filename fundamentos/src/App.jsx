7d18172e806343c1e7f5cc91ce929f9b7d7814fb
bzt <vitorcastrobuzato@gmail.com>
2024-08-25 23:54:06 -0300

    started react course

---
+++ fundamentos/src/App.jsx
@@ -0,0 +1,14 @@
+import FirstComponent from './components/FirstComponent'
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2>Hello world</h2>
+      <FirstComponent/>
+    </div>
+  )
+}
+
+export default App
