94365134d5512d384485ff946b500bd6f60529d9
vbzt <vitorcastrobuzato@gmail.com>
2024-09-25 13:31:53 -0300

    css

---
+++ css/components/MyComponent.jsx
@@ -0,0 +1,11 @@
+import './MyComponent.css'
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h2>Css no component</h2>
+    </div>
+  )
+}
+
+export default MyComponent
---
+++ css/src/App.jsx
@@ -0,0 +1,14 @@
+import MyComponent from '../components/MyComponent'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>React com css</h1>
+      <MyComponent></MyComponent>
+  </div>
+  )
+}
+
+export default App
