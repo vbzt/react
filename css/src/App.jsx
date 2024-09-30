c040749c9d74249a46a2e039a79c851ecfc36aa5
vbzt <vitorcastrobuzato@gmail.com>
2024-09-30 13:34:13 -0300

    #

--- css/src/App.jsx
+++ css/src/App.jsx
@@ -1,12 +1,18 @@
-import MyComponent from '../components/MyComponent'
+import MyComponent from './components/MyComponent'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [ counter, setCounter ] = useState(0)
 
   return (
     <div>
       <h1>React com css</h1>
       <MyComponent></MyComponent>
+      <button onClick={() =>  setCounter((counter) => counter + 1) }>Inline dinamico</button>
+      <p>Valor: {counter}</p>
+      <h3 style={counter < 10 ? ({color: 'white'}) : ({color: 'blueviolet'}) }>Eu mudo de cor quando o counter chega em 10!</h3>
+      
   </div>
   )
 }
