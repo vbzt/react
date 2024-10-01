d847d637ebcc3e328a20ac1602a9d6b35af32d1f
vbzt <vitorcastrobuzato@gmail.com>
2024-10-01 08:31:26 -0300

    finished

--- css/src/App.jsx
+++ css/src/App.jsx
@@ -1,17 +1,27 @@
 import MyComponent from './components/MyComponent'
+import Title from './components/Title'
 import './App.css'
 import { useState } from 'react'
 
 function App() {
   const [ counter, setCounter ] = useState(0)
+  const [ redTitle, setClass] = useState(true) 
+
 
   return (
     <div>
       <h1>React com css</h1>
       <MyComponent></MyComponent>
+
       <button onClick={() =>  setCounter((counter) => counter + 1) }>Inline dinamico</button>
       <p>Valor: {counter}</p>
       <h3 style={counter < 10 ? ({color: 'white'}) : ({color: 'blueviolet'}) }>Eu mudo de cor quando o counter chega em 10!</h3>
+
+    
+     <button onClick={() =>  setClass((redTitle) => redTitle ? false : true) }>Classe dinamico</button>
+     <h3 className= { redTitle ? "redTitle" : "title"}>Classe dinamica com css</h3>
+
+    <Title></Title>
       
   </div>
   )
